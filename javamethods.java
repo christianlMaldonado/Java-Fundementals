a1c1a2437debbf165f03a9f7494df8b4f0cf221c
Christian Maldonado <41269975+christianlMaldonado@users.noreply.github.com>
2021-01-27 17:40:44 -0800

    inital commit

---
+++ javamethods.java
@@ -0,0 +1,30 @@
+// Java Mehtods 
+// This will be java methods and fundementals for backend ready java developmentß
+
+
+// Method 1 is printing out first item of an array
+
+public class Main {
+    public void log (int[] numbers) {
+        // 0(1)
+        system.out.printLn(numbers[0]);
+    }
+}
+
+// looping through an array 
+public class Main {
+    public void log (int[] numbers) {
+        // 0(1)
+        for (int i = 0; i<numbers.length; i++)
+        system.out.printLn(numbers[0]);
+    }
+}
+
+// another way of loopinngs through loop 
+public class Main {
+    public void log (int[] numbers) {
+        // 0(1)
+        for (int i = 0; i<numbers.length; i++)
+        system.out.printLn(numbers[0]);
+    }
+}
